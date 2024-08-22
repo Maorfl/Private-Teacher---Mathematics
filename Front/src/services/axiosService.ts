f584808c380d3638e55cc46cdf53af00ed026a79
Maorfl <maorfl14@gmail.com>
2024-08-22 20:10:27 +0300

    meow18

--- Front/src/services/axiosService.ts
+++ Front/src/services/axiosService.ts
@@ -1,11 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import Axios, { AxiosRequestConfig } from 'axios';
+import Axios, { AxiosRequestConfig } from "axios";
 
-const BASE_URL = process.env.NODE_ENV === 'production' ?
-    '/api' :
-    'http://localhost:3000/api';
-
-
+const BASE_URL = "/api";
 
 const axios = Axios.create({
     withCredentials: true,
@@ -20,26 +16,26 @@ interface axiosService {
 
 const axiosService: axiosService = {
     async get<T>(endpoint: string, data?: any): Promise<T> {
-        return ajax<T>(endpoint, 'GET', data);
+        return ajax<T>(endpoint, "GET", data);
     },
     async post<T>(endpoint: string, data?: any): Promise<T> {
-        return ajax<T>(endpoint, 'POST', data);
+        return ajax<T>(endpoint, "POST", data);
     },
     async put<T>(endpoint: string, data?: any): Promise<T> {
-        return ajax<T>(endpoint, 'PUT', data);
+        return ajax<T>(endpoint, "PUT", data);
     },
     async delete<T>(endpoint: string, data?: any): Promise<T> {
-        return ajax<T>(endpoint, 'DELETE', data);
+        return ajax<T>(endpoint, "DELETE", data);
     },
 };
 
-async function ajax<T>(endpoint: string, method: string = 'GET', data: any = null): Promise<T> {
+async function ajax<T>(endpoint: string, method: string = "GET", data: any = null): Promise<T> {
     try {
         const config: AxiosRequestConfig = {
             url: `${BASE_URL}${endpoint}`,
             method,
             data,
-            params: (method === 'GET') ? data : null,
+            params: method === "GET" ? data : null,
         };
         const res = await axios(config);
         return res.data;
@@ -47,7 +43,7 @@ async function ajax<T>(endpoint: string, method: string = 'GET', data: any = nul
         console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data:`, data);
         console.dir(err);
         if (err.response && err.response.status === 401) {
-            window.location.href = '/login';
+            window.location.href = "/login";
         }
         throw err;
     }
