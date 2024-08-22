405f97f1d6bdfa287554ddaa01966d2ad7716e8b
Maorfl <maorfl14@gmail.com>
2024-08-22 18:26:38 +0300

    meow12

--- Back/util.service.js
+++ Back/util.service.js
@@ -2,7 +2,7 @@ function isHalfHourMoreThanNow(dateTimeString) {
     const date = new Date(dateTimeString.replace(" ", "T") + ":00Z");
     const now = new Date();
     now.setHours(now.getHours() + 3);
-    console.log(date.getTime() - now.getTime() <= 30 * 60000 && date.getTime() - now.getTime() >= 25 * 60000);
+    console.log(date.getTime() - now.getTime() <= 35 * 60000 && date.getTime() - now.getTime() >= 25 * 60000);
 
     if (date.getTime() - now.getTime() <= 30 * 60000 && date.getTime() - now.getTime() >= 25 * 60000) return true;
     return false;
